8c91a7afff71dd4f763f32c2c2656c313e786d65
playdog-io <cqdxcs3@163.com>
2021-08-30 15:00:58 +0800

    入门课5-3笔记做完,可以在前端实现poe功能,还未添加测试相关代码

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -9,45 +9,57 @@ mod tests;
 
 #[frame_support::pallet]
 pub mod pallet {
+    // aaa1
     use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
+    // aaa1,0740 引入相关依赖
     use sp_std::vec::Vec;
 
+    // aaa2
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
+    // aaa2,0745 定义模块配置接口
 
+    // aaa3
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
-
     pub struct Pallet<T>(_);
+    // aaa3,0820 定义pallet结构体承担功能模块
 
+    // aaa4
     #[pallet::storage]
     #[pallet::getter(fn proofs)]
-
     pub type Proofs<T: Config> =
         StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber)>;
+    // aaa4,0840 定义存储单元proofs用来存储存证.这里只有一个存储单元.
 
+    // aaa5
     #[pallet::event]
     #[pallet::metadata(T::AccountId = "AccountId")]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
-
     pub enum Event<T: Config> {
         ClaimCreated(T::AccountId, Vec<u8>),
         ClaimRevoked(T::AccountId, Vec<u8>),
     }
+    // aaa5,0920 定义event枚举类型,此时ClaimCreated和ClaimRevoked还未写入.
 
+    // aaa6
     #[pallet::error]
     pub enum Error<T> {
         ProofAlreadyExist,
         ClaimNotExist,
         NotClaimOwner,
     }
+    // aaa6,0940 定义error枚举来包含error信息.此时相关代码还未写入.
 
+    // aaa7
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    // aaa7,0955 定义区块某时刻执行的特殊函数放在hooks里.但此pallet不需要,所以无内容.
 
+    // aaa8
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::weight(0)]
@@ -103,3 +115,9 @@ pub mod pallet {
         }
     }
 }
+    // aaa8,1030 定义可调用函数
+    /// aaa9,1400e 讲解了8中的创建存证相关内容. 
+    /// aaa10, 1711e 讲解了8中吊销存证相关内容.
+
+    ///aaa11,1718-2145 讲解了在runtime里引入poe.并build后运行.
+    ///aaa12,2145-2524 演示前端互动
